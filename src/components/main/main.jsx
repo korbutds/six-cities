05bef8714728e6e84de17d034539b71349604365
korbutds <sys@hohou.ru>
2021-02-03 16:51:39 +0300

    feat: Добавлен подсчет существующих объявлений

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -70,7 +70,7 @@ const MainScreen = ({cards}) => {
           <div className="cities__places-container container">
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">312 places to stay in Amsterdam</b>
+              <b className="places__found">{cards.length} places to stay in Amsterdam</b>
               <form className="places__sorting" action="#" method="get">
                 <span className="places__sorting-caption">Sort by</span>
                 <span className="places__sorting-type" tabIndex="0">
