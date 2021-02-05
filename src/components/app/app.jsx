bdacfb551904720544daba0a3a01aa09db23c6a9
korbutds <sys@hohou.ru>
2021-02-05 12:00:15 +0300

    feat: Созданы компоненты отставшихся страниц

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import {cardsPropTypes} from '../../prop-types';
-
-
 import MainScreen from '../main/main';
+import LoginScreen from '../login/login';
+import FavoritesScreen from '../favorites/favorites';
+import PropertyScreen from '../property/property';
 
 const App = ({cards}) => (
-  <MainScreen
-    cards = {cards}
-  />
+  <>
+    <LoginScreen />
+    <MainScreen
+      cards = {cards}
+    />
+    <FavoritesScreen />
+    <PropertyScreen />
+  </>
 );
 
 
---
+++ src/components/login/login.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+const LoginScreen = () => (
+  <div className="page page--gray page--login">
+    <header className="header">
+      <div className="container">
+        <div className="header__wrapper">
+          <div className="header__left">
+            <a className="header__logo-link" href="main.html">
+              <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
+            </a>
+          </div>
+          <nav className="header__nav">
+            <ul className="header__nav-list">
+              <li className="header__nav-item user">
+                <a className="header__nav-link header__nav-link--profile" href="#">
+                  <div className="header__avatar-wrapper user__avatar-wrapper">
+                  </div>
+                  <span className="header__login">Sign in</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+
+    <main className="page__main page__main--login">
+      <div className="page__login-container container">
+        <section className="login">
+          <h1 className="login__title">Sign in</h1>
+          <form className="login__form form" action="#" method="post">
+            <div className="login__input-wrapper form__input-wrapper">
+              <label className="visually-hidden">E-mail</label>
+              <input className="login__input form__input" type="email" name="email" placeholder="Email" required="" />
+            </div>
+            <div className="login__input-wrapper form__input-wrapper">
+              <label className="visually-hidden">Password</label>
+              <input className="login__input form__input" type="password" name="password" placeholder="Password" required="" />
+            </div>
+            <button className="login__submit form__submit button" type="submit">Sign in</button>
+          </form>
+        </section>
+        <section className="locations locations--login locations--current">
+          <div className="locations__item">
+            <a className="locations__item-link" href="#">
+              <span>Amsterdam</span>
+            </a>
+          </div>
+        </section>
+      </div>
+    </main>
+  </div>
+);
+
+export default LoginScreen;
