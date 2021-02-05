a2f5c64837c72a2f295a5a572135c45d423d6631
korbutds <sys@hohou.ru>
2021-02-05 18:52:59 +0300

    feat: Добавил роутинг

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,22 +1,43 @@
 import React from 'react';
+import {Switch, Route, BrowserRouter as Router} from 'react-router-dom';
 import {cardsPropTypes} from '../../prop-types';
 import MainScreen from '../main/main';
 import LoginScreen from '../login/login';
 import FavoritesScreen from '../favorites/favorites';
 import PropertyScreen from '../property/property';
-import NotFoundScreen from '../404/404';
+import NotFoundScreen from '../not-found/not-found';
 
-const App = ({cards}) => (
-  <>
-    <LoginScreen />
-    <MainScreen
-      cards = {cards}
-    />
-    <FavoritesScreen />
-    <PropertyScreen />
-    <NotFoundScreen />
-  </>
-);
+const App = ({cards}) => {
+  return (
+    <Router>
+      <Switch>
+
+        <Route path="/" exact>
+          <MainScreen
+            cards = {cards}
+          />
+        </Route>
+
+        <Route path="/login" exact>
+          <LoginScreen />
+        </Route>
+
+        <Route path="/favorites" exact>
+          <FavoritesScreen />
+        </Route>
+
+        <Route path="/offer/:id" exact>
+          <PropertyScreen />
+        </Route>
+
+        <Route>
+          <NotFoundScreen />
+        </Route>
+
+      </Switch>
+    </Router>
+  );
+};
 
 
 App.propTypes = {
--- src/components/not-found/not-found.jsx
+++ src/components/not-found/not-found.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const NotFoundScreen = () => (
-  <div className="page">
+  <div className="page page--gray page--main">
     <header className="header">
       <div className="container">
         <div className="header__wrapper">
           <div className="header__left">
-            <a className="header__logo-link" href="main.html">
+            <a className="header__logo-link header__logo-link--active">
               <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
             </a>
           </div>
@@ -25,22 +25,20 @@ const NotFoundScreen = () => (
       </div>
     </header>
 
-    <main className="page__main page__main--favorites page__main--favorites-empty">
-      <div className="container page__favorites-container">
-        <section className="favorites favorites--empty">
-          <h1 className="visually-hidden">Page Not Found</h1>
-          <div className="favorites__status-wrapper"
-            style= {{
-              backgroundImage: `none`
-            }}
-          >
-            <b className="favorites__status">404.</b>
-            <p className="favorites__status-description">Page not found.</p>
-          </div>
-        </section>
+    <main className="page__main page__main--index page__main--index-empty">
+      <h1 className="visually-hidden">Cities</h1>
+      <div className="cities">
+        <div className="cities__places-container cities__places-container--empty container">
+          <section className="cities__no-places">
+            <div className="cities__status-wrapper tabs__content">
+              <b className="cities__status">404</b>
+              <p className="cities__status-description">We are sorry, but page not found</p>
+            </div>
+          </section>
+          <div className="cities__right-section"></div>
+        </div>
       </div>
     </main>
-
   </div>
 );
 
