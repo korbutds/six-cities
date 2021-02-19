cbf1ac0385916defa253fa1e1355fe176f17a28b
korbutds <sys@hohou.ru>
2021-02-19 16:09:56 +0300

    feat: Устанавливате пакеты leaf

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     devServer: {
         contentBase: path.resolve(__dirname, 'public'),
-        open: false,
+        open: true,
         port: 1337,
         historyApiFallback: true,
     },
