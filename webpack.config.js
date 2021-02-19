810000302415c0b6e1a572fd5f55cc20d2075c5e
korbutds <sys@hohou.ru>
2021-02-19 16:12:24 +0300

    feat: Настраивает webpack для работы с leaflet

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,14 @@ module.exports = {
             use: {
             loader: 'babel-loader',
             },
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: ['file-loader'],
         }
         ],
     },
