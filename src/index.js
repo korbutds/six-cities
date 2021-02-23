31c395199712b1b4cdd320d847112c3409432eee
korbutds <sys@hohou.ru>
2021-02-23 15:08:18 +0300

    feat: Подключает redux к проекту.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
 import App from '../src/components/app/app';
 import {Offers} from '../src/mocks/offers.js';
+import {Provider} from 'react-redux';
+import {reducer} from './store/reducer';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-    <App
-      cards = {Offers}
-    />,
+    <Provider store={store}>
+      <App
+        cards = {Offers}
+      />
+    </Provider>,
     document.querySelector(`#root`)
 );
 
