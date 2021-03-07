d4c8e42c972ba0ce63da8135df2a082591a012f2
korbutds <sys@hohou.ru>
2021-03-07 18:23:36 +0300

    fix: Решает проблему с обновление комментария компонента

--- src/components/comment-form/comment-form.jsx
+++ src/components/comment-form/comment-form.jsx
@@ -80,7 +80,7 @@ const CommentForm = ({id, onSubmit}) => {
         <p className="reviews__help">
           To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
         </p>
-        <button className="reviews__submit form__submit button" type="submit" disabled="">Submit</button>
+        <button className="reviews__submit form__submit button" type="submit" disabled={(comment.rating && comment.commentText) ? false : true} >Submit</button>
       </div>
     </form>
   );
