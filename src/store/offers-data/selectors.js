881afadaa9e4fd78060f12428e776f9d60617f1e
korbutds <sys@hohou.ru>
2021-03-11 20:25:05 +0300

    refactor: Удалит селекторы

--- src/store/offers-data/selectors.js
+++
@@ -1,6 +0,0 @@
-import {NameSpace} from "../root-reducer";
-
-export const getOffersCards = (state) => state[NameSpace.DATA].cards;
-export const getCardsLoadedStatus = (state) => state[NameSpace.DATA].isCardsLoaded;
-
-
--- src/store/user-data/selectors.js
+++
@@ -1,5 +0,0 @@
-import {NameSpace} from "../root-reducer";
-
-export const getAuthorisationStatus = (state) => state[NameSpace.USER].authorizationStatus;
-export const getUserLogin = (state) => state[NameSpace.USER].login;
-export const getUserAvatar = (state) => state[NameSpace.USER].userAvatar;
