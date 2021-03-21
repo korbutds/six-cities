2d141181f130f7c902c07390ead4433dc2231287
korbutds <sys@hohou.ru>
2021-03-21 22:11:16 +0300

    feat: Добаляет тесты для тестирования компонента NoPlaces

--- src/components/no-places/no-places.snap.test.js
+++ src/components/no-places/no-places.snap.test.js
@@ -3,6 +3,7 @@ import {render} from '@testing-library/react';
 import NoPlaces from './no-places';
 
 test(`Should render NoPlaces component correctly`, () => {
-  const {container} = render(<NoPlaces/>);
-  expect(container).toMatchSnapshot();
+  const {getByText} = render(<NoPlaces/>);
+  const citiesStatus = getByText(`No places to stay available`);
+  expect(citiesStatus).toBeInTheDocument();
 });
